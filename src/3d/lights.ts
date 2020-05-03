e260240f530108eac490acbcab6e0425011536d1
Enrico Graziani <enrico.graziani@cazoo.co.uk>
2020-05-03 17:19:46 +0100

    WIP added initial assets for testing

--- src/3d/lights.ts
+++ src/3d/lights.ts
@@ -1,6 +1,9 @@
-import {Light, PointLight, Scene, Vector3} from "@babylonjs/core";
+import {HemisphericLight, Light, PointLight, Scene, Vector3} from "@babylonjs/core";
 import {ShadowLight} from "@babylonjs/core/Lights/shadowLight";
 
-export function addTo(scene: Scene): ShadowLight {
-    return new PointLight("pointLight", new Vector3(0, 999, 0), scene);
+export function addTo(scene: Scene): HemisphericLight {
+    const light = new HemisphericLight("hemisphericLight", new Vector3(0, 1, 0), scene);
+    // const light = new PointLight("pointLight", new Vector3(0, 999, 0), scene);
+    // light.intensity = 9999999;
+    return light;
 }
--- src/3d/scene.ts
+++ src/3d/scene.ts
@@ -4,6 +4,7 @@ import {
     Vector3,
     Mesh, ShadowGenerator, Engine, Color4
 } from "@babylonjs/core";
+import  "@babylonjs/loaders/glTF";
 
 import {SkyMaterial} from '@babylonjs/materials/sky';
 import * as lights from "./lights";
@@ -21,40 +22,46 @@ function setupScene(engine: Engine): Scene {
     return scene;
 }
 
+function importMesh(scene: Scene, filename: string ) {
+    SceneLoader.ImportMesh(
+        "",
+        "/asset/",
+        filename,
+        scene,
+        (objs) => {
+            objs.forEach(obj => {
+                console.log("Imported ", obj.id, obj.name);
+                obj.checkCollisions = true;
+                obj.receiveShadows = true;
+                // shadowGenerator?.getShadowMap()?.renderList?.push(obj);
+            });
+        }, (event) => {
+            console.log(event);
+        }, (scene, error) => {
+            console.log(error)
+        });
+}
+
 export function createScene(engine: Engine, canvas: HTMLCanvasElement): Scene {
     const scene = setupScene(engine);
     const light = lights.addTo(scene);
-    const shadowGenerator = new ShadowGenerator(1024, light);
+    // const shadowGenerator = new ShadowGenerator(1024, light);
 
     getCamera(scene, canvas);
-    getGround(scene);
+    // getGround(scene);
 
-    SceneLoader.ImportMesh(
-        "dragon",
-        "https://models.babylonjs.com/Georgia-Tech-Dragon/",
-        "dragon.babylon",
-        scene,
-        (objs) => {
-            const dragon = objs[0]
-
-            dragon.scaling = new Vector3(100, 100, 100)
-            dragon.position = new Vector3(30, 0, -50);
-            
-            shadowGenerator?.getShadowMap()?.renderList?.push(dragon);
-            shadowGenerator.usePoissonSampling = true; // TODO check performance
-            
-            // @ts-ignore TODO REMOVE THIS!!!
-            window.dragon = dragon;
-        });
+    ["Bar decimated.glb", "Perimeter.glb", "Sofa decimated.glb", "Stool decimated compressed.glb", "Stool decimated.glb", "Tent.glb", "Tree decimated.glb", "Umbrella.glb"].forEach(filename => importMesh(scene, filename));
+    
+    // shadowGenerator.usePoissonSampling = true; // TODO check performance
 
     scene.clearColor = new Color4(80 / 256, 166 / 256, 255 / 256, 1);
 
     const skyMaterial = new SkyMaterial("skyMaterial", scene);
-    skyMaterial.backFaceCulling = false;
     skyMaterial.luminance = 1;
     skyMaterial.inclination = 0;
     skyMaterial.turbidity = 40;
-    const skybox = Mesh.CreateBox("skyBox", 1000.0, scene);
+    skyMaterial.backFaceCulling = false;
+    const skybox = Mesh.CreateBox("skyBox", 1000, scene);
     skybox.material = skyMaterial;
 
     return scene;
