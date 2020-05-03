8054371ae9738ab6080155924c57e0754a118669
Enrico Graziani <enrico.graziani@cazoo.co.uk>
2020-05-03 20:51:55 +0100

    cleaned up code

--- src/3d/lights.ts
+++ src/3d/lights.ts
@@ -1,9 +1,10 @@
-import {HemisphericLight, Light, PointLight, Scene, Vector3} from "@babylonjs/core";
+import {HemisphericLight, PointLight, Scene, Vector3} from "@babylonjs/core";
 import {ShadowLight} from "@babylonjs/core/Lights/shadowLight";
 
-export function addTo(scene: Scene): HemisphericLight {
-    const light = new HemisphericLight("hemisphericLight", new Vector3(0, 1, 0), scene);
-    // const light = new PointLight("pointLight", new Vector3(0, 999, 0), scene);
-    // light.intensity = 9999999;
-    return light;
+export function addTo(scene: Scene): ShadowLight {
+    const ambientLight = new HemisphericLight("hemisphericLight", new Vector3(0, 1, 0), scene);
+    ambientLight.intensity = 0.3;
+    const sunLight = new PointLight("pointLight", new Vector3(0, 999, 0), scene);
+    sunLight.intensity = 2999999;
+    return sunLight;
 }
--- src/3d/scene.ts
+++ src/3d/scene.ts
@@ -1,18 +1,18 @@
-import {
-    Scene,
-    SceneLoader,
-    Vector3,
-    Mesh, ShadowGenerator, Engine, Color4
-} from "@babylonjs/core";
-import  "@babylonjs/loaders/glTF";
+// TODO disable inspector in production!!!
+// import "@babylonjs/core/Debug/debugLayer";
+// import "@babylonjs/inspector";
 
-import {SkyMaterial} from '@babylonjs/materials/sky';
+import {Color4, Engine, Scene, ShadowGenerator, Vector3} from "@babylonjs/core";
+
 import * as lights from "./lights";
-import {getCamera} from "./camera";
-import {getGround} from "./ground";
+import {createCamera} from "./camera";
+import {importMesh} from "./mesh-importer";
+import {createSky} from "./sky";
 
 function setupScene(engine: Engine): Scene {
     const scene = new Scene(engine);
+    scene.clearColor = new Color4(80 / 256, 166 / 256, 255 / 256, 1);
+    
     //Set gravity for the scene (G force like, on Y-axis)
     scene.gravity = new Vector3(0, -0.9, 0);
 
@@ -22,47 +22,16 @@ function setupScene(engine: Engine): Scene {
     return scene;
 }
 
-function importMesh(scene: Scene, filename: string ) {
-    SceneLoader.ImportMesh(
-        "",
-        "/asset/",
-        filename,
-        scene,
-        (objs) => {
-            objs.forEach(obj => {
-                console.log("Imported ", obj.id, obj.name);
-                obj.checkCollisions = true;
-                obj.receiveShadows = true;
-                // shadowGenerator?.getShadowMap()?.renderList?.push(obj);
-            });
-        }, (event) => {
-            console.log(event);
-        }, (scene, error) => {
-            console.log(error)
-        });
-}
-
 export function createScene(engine: Engine, canvas: HTMLCanvasElement): Scene {
     const scene = setupScene(engine);
     const light = lights.addTo(scene);
-    // const shadowGenerator = new ShadowGenerator(1024, light);
-
-    getCamera(scene, canvas);
-    // getGround(scene);
-
-    ["Bar decimated.glb", "Perimeter.glb", "Sofa decimated.glb", "Stool decimated compressed.glb", "Stool decimated.glb", "Tent.glb", "Tree decimated.glb", "Umbrella.glb"].forEach(filename => importMesh(scene, filename));
+    const shadowGenerator = new ShadowGenerator(1024, light);
+    createSky(scene);
+    createCamera(scene, canvas);
     
-    // shadowGenerator.usePoissonSampling = true; // TODO check performance
-
-    scene.clearColor = new Color4(80 / 256, 166 / 256, 255 / 256, 1);
-
-    const skyMaterial = new SkyMaterial("skyMaterial", scene);
-    skyMaterial.luminance = 1;
-    skyMaterial.inclination = 0;
-    skyMaterial.turbidity = 40;
-    skyMaterial.backFaceCulling = false;
-    const skybox = Mesh.CreateBox("skyBox", 1000, scene);
-    skybox.material = skyMaterial;
-
+    ["Bar decimated.glb", "Perimeter.glb", "Sofa decimated.glb", "Stool decimated compressed.glb", "Tent.glb", "Tree decimated.glb", "Umbrella.glb"]
+        .forEach(filename => importMesh(scene, filename, shadowGenerator));
+    
+    // scene.debugLayer.show()
     return scene;
 }
