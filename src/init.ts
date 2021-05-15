a406859ef1c21f9aa989e7704fb080beba9474b7
Enrico Graziani <enrico.graziani@cazoo.co.uk>
2021-05-15 17:33:27 +0100

    green on spin up virtual world + showWebcamStream

--- src/init.ts
+++ src/init.ts
@@ -1,16 +1,27 @@
 export class Room {
-    constructor(private local: Local, private remoteRoom: RemoteRoom) {}
+    constructor(
+        private local: Local,
+        private remoteRoom: RemoteRoom,
+        private virtualWord: VirtualWorld
+    ) {}
 
     async join(_room: string): Promise<void> {
         await this.local.getWebcamStream();
+        await this.local.showWebcamStream();
         await this.remoteRoom.join();
+        await this.virtualWord.start();
     }
 }
 
 export interface Local {
+    showWebcamStream: () => void;
     getWebcamStream: () => Promise<void>;
 }
 
 export interface RemoteRoom {
     join: () => Promise<void>;
+}
+
+export interface VirtualWorld {
+    start: () => Promise<void>;
 }
--- tests/unit/init.test.ts
+++ tests/unit/init.test.ts
@@ -1,4 +1,4 @@
-import { Local, RemoteRoom, Room } from "../../src/init";
+import { Local, RemoteRoom, Room, VirtualWorld } from "../../src/init";
 
 describe("when entering a room", () => {
     const remoteRoom: RemoteRoom = {
@@ -6,11 +6,16 @@ describe("when entering a room", () => {
     };
 
     const local: Local = {
+        showWebcamStream: jest.fn(),
         getWebcamStream: jest.fn().mockResolvedValue(undefined),
     };
 
+    const virtualWord: VirtualWorld = {
+        start: jest.fn().mockResolvedValue(undefined),
+    };
+
     beforeEach(async () => {
-        const room = new Room(local, remoteRoom);
+        const room = new Room(local, remoteRoom, virtualWord);
         await room.join("aRoom");
     });
 
@@ -18,7 +23,15 @@ describe("when entering a room", () => {
         expect(local.getWebcamStream).toHaveBeenCalled();
     });
 
+    it("show the local webcam video to the user", async () => {
+        expect(local.showWebcamStream).toHaveBeenCalled();
+    });
+
     it("join remote room", async () => {
         expect(remoteRoom.join).toHaveBeenCalled();
+    });
+
+    it("spin up the virtual world", () => {
+        expect(virtualWord.start).toHaveBeenCalled();
     });
 });
