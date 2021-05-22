6b8ab7d34d7669c47ac73b66f25333e4dfe2ab13
Enrico Graziani <enrico.graziani@cazoo.co.uk>
2021-05-22 11:46:10 +0100

    refactored out of new-stuff

--- webpack.common.js
+++ webpack.common.js
@@ -9,7 +9,7 @@ const Dotenv = require('dotenv-webpack');
 const appDirectory = fs.realpathSync(process.cwd());
 
 module.exports = {
-    entry: path.resolve(appDirectory, "src/new-stuff"),
+    entry: path.resolve(appDirectory, "src"),
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, 'public/dist')
