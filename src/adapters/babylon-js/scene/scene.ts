8707cfd43415a331c26f97939b93d9fc052f0ec5
Enrico Graziani <enrico.graziani@cazoo.co.uk>
2021-05-29 18:51:39 +0100

    added ground

--- src/adapters/babylon-js/scene/scene.ts
+++ src/adapters/babylon-js/scene/scene.ts
@@ -2,7 +2,15 @@
 // import "@babylonjs/core/Debug/debugLayer";
 // import "@babylonjs/inspector";
 
-import { Color4, Engine, Scene, Vector3 } from "@babylonjs/core";
+import {
+    Color3,
+    Color4,
+    Engine,
+    Mesh,
+    PBRMaterial,
+    Scene,
+    Vector3,
+} from "@babylonjs/core";
 
 import * as lights from "./lights";
 import { createCamera } from "./camera";
@@ -22,13 +30,33 @@ function setupScene(engine: Engine): Scene {
     return scene;
 }
 
+function addGround(scene: Scene) {
+    const ground = Mesh.CreateGround("ground", 100, 100, 2, scene);
+    const groundMaterial = new PBRMaterial("groundMaterial", scene);
+    groundMaterial.albedoColor = Color3.FromHexString("#1b5b1a");
+    groundMaterial.metallic = 0.01;
+    groundMaterial.backFaceCulling = false;
+    ground.material = groundMaterial;
+    ground.checkCollisions = true;
+    ground.position.y = -1;
+    // hidden ground
+    const seaGround = Mesh.CreateGround("seaGround", 500, 500, 2, scene);
+    const seaGroundMaterial = new PBRMaterial("seaGroundMaterial", scene);
+    seaGroundMaterial.albedoColor = Color3.FromHexString("#22A2F8");
+    seaGroundMaterial.metallic = 0.01;
+    seaGroundMaterial.backFaceCulling = false;
+    seaGround.material = seaGroundMaterial;
+    seaGround.checkCollisions = true;
+    seaGround.position.y = -25;
+}
+
 export function createScene(engine: Engine, canvas: HTMLCanvasElement): Scene {
     const scene = setupScene(engine);
     lights.addTo(scene);
     createSky(scene);
     createCamera(scene, canvas);
     loadAssets(scene);
-
+    addGround(scene);
     // scene.debugLayer.show()
     return scene;
 }
