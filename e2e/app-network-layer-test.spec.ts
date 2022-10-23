a914a5fc31c0bbd472c8598c539152e8233d5c5b
Enrico Graziani <mrenrich84@gmail.com>
2022-10-24 00:57:11 +0100

    playwright works well for 3 browsers

--- e2e/app-network-layer-test.spec.ts
+++ e2e/app-network-layer-test.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { Browser, expect, Page, test } from "@playwright/test";
 
 test.use({
     permissions: ["microphone", "camera"],
@@ -11,49 +11,47 @@ test.use({
     },
 });
 
+const openNewPage = async (browser: Browser) => {
+    const page = await browser.newContext().then((c) => c.newPage());
+    await page.goto("https://localhost:9000/remote-room-tester.html");
+    return page;
+};
+
+async function checkPeerIsVisible(userPage: Page, peer: string) {
+    const peerName = userPage.getByText(peer);
+    await expect(peerName).toBeVisible();
+    const peerId = await peerName
+        .evaluate((n) => n.id)
+        .then((s) => s.replace("name-", ""));
+    const peerVideoSrcOb = await userPage
+        .locator(`#video-${peerId}`)
+        .evaluate((n) => (n as HTMLVideoElement).srcObject);
+    expect(peerVideoSrcOb).not.toBeNull();
+}
+
 test("app network layer", async ({ browser }) => {
-    const aliceBrowser = await browser.newContext();
-    const alicePage = await aliceBrowser.newPage();
-    await alicePage.goto("https://localhost:9000/remote-room-tester.html");
+    const alicePage = await openNewPage(browser);
     await alicePage.locator("#name").fill("alice");
     await alicePage.locator("#testRemoteRoom").click();
     await expect(alicePage.locator("#type")).toHaveValue("peer");
 
     await expect(alicePage.getByText("bob")).not.toBeVisible();
 
-    const bobBrowser = await browser.newContext();
-    const bobPage = await bobBrowser.newPage();
-    await bobPage.goto("https://localhost:9000/remote-room-tester.html");
+    const bobPage = await openNewPage(browser);
     await bobPage.locator("#name").fill("bob");
     await bobPage.locator("#testRemoteRoom").click();
     await expect(bobPage.locator("#type")).toHaveValue("peer");
 
-    const aliceName = bobPage.getByText("alice");
-    await expect(aliceName).toBeVisible();
-    const aliceId = await aliceName
-        .evaluate((n) => n.id)
-        .then((s) => s.replace("name-", ""));
-    const aliceVideoSrcOb = await bobPage
-        .locator(`#video-${aliceId}`)
-        .evaluate((n) => (n as HTMLVideoElement).srcObject);
-    expect(aliceVideoSrcOb).not.toBeNull();
+    await checkPeerIsVisible(bobPage, "alice");
+    await checkPeerIsVisible(alicePage, "bob");
 
-    const bobName = alicePage.getByText("bob");
-    await expect(bobName).toBeVisible();
-    const bobId = await bobName
-        .evaluate((n) => n.id)
-        .then((s) => s.replace("name-", ""));
-    const bobVideoSrcOb = await alicePage
-        .locator(`#video-${bobId}`)
-        .evaluate((n) => (n as HTMLVideoElement).srcObject);
-    expect(bobVideoSrcOb).not.toBeNull();
-
-    const charlieBrowser = await browser.newContext();
-    const charliePage = await charlieBrowser.newPage();
-    await charliePage.goto("https://localhost:9000/remote-room-tester.html");
+    const charliePage = await openNewPage(browser);
     await charliePage.locator("#name").fill("charlie");
     await charliePage.locator("#testRemoteRoom").click();
     await expect(charliePage.locator("#type")).toHaveValue("peer");
 
-    // await new Promise(() => {});
+    await checkPeerIsVisible(charliePage, "alice");
+    await checkPeerIsVisible(charliePage, "bob");
+    await checkPeerIsVisible(bobPage, "charlie");
+    await checkPeerIsVisible(alicePage, "charlie");
 });
